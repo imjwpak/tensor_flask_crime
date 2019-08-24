f8b7f2f2795e0042ef46aee7fe59a089a069565e
imjwpak83 <imjwpak83@naver.com>
2019-08-24 17:07:56 +0900

    17:07

--- seoul_crime/crime_police.py
+++ seoul_crime/crime_police.py
@@ -13,13 +13,38 @@ class CrimeModel:
 
         self.dr.fname = 'crime_in_seoul.csv'
         crime = self.dr.csv_to_dframe()
-        print(crime)
-
+        
         station_names = []
-        for name in creim['관서명']:
+        for name in crime['관서명']:
             station_names.append('서울' + str(name[:-1] + '경찰서'))
             
         station_addrs = []
         station_lats = [] # 위도
         station_lngs = [] # 경도
         gmaps = self.dr.create_gmaps()
+        for name in station_names:
+            t = gmaps.geocode(name, language='ko')
+            station_addrs.append(t[0].get('formatted_address'))
+            t_loc = t[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
+            print(name + '---------> ' + t[0].get('formatted_address'))
+
+        gu_names = []
+        for name in statino_addrs:
+            t = name.split()
+            gu_name = [gu for gu in t if gu[-1] == '구'][0]
+            gu_names.append(gu_name)
+        crime['구별'] = gu_names
+
+        # 구와 경찰서 위치가 다른 경우 수작업
+        crime.loc[crime['관서명'] == '혜화서', ['구별']] == '종로구'
+        crime.loc[crime['관서명'] == '서부서', ['구별']] == '은평구'
+        crime.loc[crime['관서명'] == '강서서', ['구별']] == '앙천구'
+        crime.loc[crime['관서명'] == '종암서', ['구별']] == '성북구'
+        crime.loc[crime['관서명'] == '방배서', ['구별']] == '서초구'
+        crime.loc[crime['관서명'] == '수서서', ['구별']] == '강남구'
+
+        print(crime)
+
+        crime.to_csv('./saved_data/crime_police.csv')
